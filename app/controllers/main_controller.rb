6fd9741d2ff707ae0dea640174ba585dc6f1372a
Nick-Brennan <nick_p_b@yahoo.com>
2016-09-28 11:52:31 -0700

    added listen view

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,10 +1,11 @@
 class MainController < ApplicationController
   skip_before_filter  :verify_authenticity_token
-  
+
   def index
   end
 
   def listen
     response.headers['realtimeapi_code'] = params['realtimeapi_code']
+    p params
   end
 end
