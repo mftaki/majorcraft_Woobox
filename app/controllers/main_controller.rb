ed0f4bf118ffbeb4e0536f1cc107a445bfeea1c6
Nick-Brennan <nick_p_b@yahoo.com>
2016-09-28 15:14:58 -0700

    trying to convert entries string into array 2

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,6 +6,6 @@ class MainController < ApplicationController
 
   def listen
     response.headers['realtimeapi_code'] = params['realtimeapi_code']
-    p params['entries'].gsub("\", "").gsub("{", "").split(":")
+    p params['entries'].gsub("\\", "").gsub("\{", "").split(":")
   end
 end
