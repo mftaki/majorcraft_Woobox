88d7d025de47062ab3015bac0af30df2cb1144ef
Nick-Brennan <nick_p_b@yahoo.com>
2016-09-29 13:37:08 -0700

    added remaining fields to push to DE

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -16,47 +16,6 @@ class MainController < ApplicationController
     if(params['entries'] != nil)
       entries = JSON.parse(params['entries'])
       p entries
-      
-      # requestToken = HTTParty.post("https://auth.exacttargetapis.com/v1/requestToken",
-      #   {
-      #   headers: {
-      #     "Content-Type" => "application/json"
-      #   },
-      #   body: {
-      #     "clientId": "7b0dwqmgp5sebmnf5fbvedwr",
-      #     "clientSecret": "dojfvyOTi28i48kyhWOCotv1"
-      #   }.to_json
-      # })
-
-      # access_token = "Bearer " + requestToken.parsed_response["accessToken"]
-
-      # new_entry = HTTParty.post("https://www.exacttargetapis.com/hub/v1/dataevents/key:03320FA9-EF5C-4300-BFEC-70D5F0EE618B/rowset",
-      #   {
-      #   headers: {
-      #     "Authorization" => access_token,
-      #     "Content-Type" => "application/json"
-      #   },
-      #   body: {[{
-      #     "keys": {
-      #       "EmailAddress": entries[0]['email']
-      #     },
-      #     "values": {
-      #       "Signup_Time": entries[0]['createdate'],
-      #       "Custom4": entries[0]['custom_4'],
-      #       "FName": entries[0]['first_name'],
-      #       "LName": entries[0]['last_name']
-      #     }
-      #   }]}.to_json
-      #   })
-
-
-      # myClient = FuelSDK::Client.new {'client':{ 'id' => "7b0dwqmgp5sebmnf5fbvedwr", 'secret' => "dojfvyOTi28i48kyhWOCotv1" }}
-      # dataextensionrow = FuelSDK::DataExtension::Row.new
-      # dataextensionrow.authStub = myClient
-      # dataextensionrow.Name = 'ExampleDEName'
-      # dataextensionrow.props = {"EmailAddress" => entries[0]['email'], "Signup_Time" => entries[0]['createdate']}
-      # results = dataextensionrow.post
-      # p results
 
       myclient = ET_Client.new auth
       p myclient
@@ -64,7 +23,18 @@ class MainController < ApplicationController
       dataextensionrow = FuelSDK::DataExtension::Row.new
       dataextensionrow.authStub = myclient
       dataextensionrow.Name = 'WOOBOX_API'
-      dataextensionrow.props = {"EmailAddress" => entries[0]['email'], "Signup_Time" => entries[0]['createdate']}
+      dataextensionrow.props = {"EmailAddress" => entries[0]['email'],
+                                "Signup_Time" => entries[0]['createdate'],
+                                "Age" => entries[0]['age'],
+                                "City" => entries[0]['city'],
+                                "Class" => entries[0]['class'],
+                                "Custom4" => entries[0]['custom_4'],
+                                "Custom5" => entries[0]['custom_5'],
+                                "FName" => entries[0]['first_name'],
+                                "LName" => entries[0]['last_name'],
+                                "Occupation" => entries[0]['occupation'],
+                                "OtherProfession" => entries[0]['other_profession'],
+                                "IP_Address" => entries[0]['ipaddress']}
       results = dataextensionrow.post
 
       p results.results
