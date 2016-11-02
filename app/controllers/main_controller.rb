6951fc4d53b6b3e1fb8708c756da0e12c81de856
Nick-Brennan <nick_p_b@yahoo.com>
2016-11-02 11:47:29 -0700

    looping through entries

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -16,56 +16,60 @@ class MainController < ApplicationController
     if(params['entries'] != nil)
       entries = JSON.parse(params['entries'])
       p entries
-      c4 = false
-      c5 = false
-
-      if(entries[0]['own_yamaha_bike'] == "Yes")
-        c4 = true
-      end
-
-      if(entries[0]['subscribe_newsletter'] == "Yes")
-        c5 = true
-      end
 
       myclient = ET_Client.new auth
       p myclient
 
-      dataextensionrow = FuelSDK::DataExtension::Row.new
-      dataextensionrow.authStub = myclient
-      dataextensionrow.Name = 'WOOBOX_API'
-      dataextensionrow.props = {"EmailAddress" => entries[0]['email'],
-                                "Signup_Time" => entries[0]['createdate'],
-                                "Age" => entries[0]['age'],
-                                "City" => entries[0]['city'],
-                                "Class" => entries[0]['class'],
-                                "Custom4" => c4,
-                                "Custom5" => c5,
-                                "FName" => entries[0]['first_name'],
-                                "LName" => entries[0]['last_name'],
-                                "Phone" => entries[0]['phone'],
-                                "Occupation" => entries[0]['occupation'],
-                                "OtherProfession" => entries[0]['other_profession'],
-                                "IP_Address" => entries[0]['ipaddress']}
-      results = dataextensionrow.post
+      entries.each do |entry|
+        c4 = false
+        c5 = false
 
-      p results.results
+        if(entry['own_yamaha_bike'] == "Yes")
+          c4 = true
+        end
 
-      myclient = ET_Client.new auth
+        if(entry[0]['subscribe_newsletter'] == "Yes")
+          c5 = true
+        end
 
-      email = entries[0]['email']
+        
 
-  # Send an email with TriggeredSend
-      p '>>> Send an email with TriggeredSend'
-      sendTrig = FuelSDK::TriggeredSend.new
-      sendTrig.authStub = myclient
-      sendTrig.props = [{"CustomerKey" => "2156", "Subscribers" => {"SubscriberKey"=> email, "EmailAddress"=> email}}]
-      sendResponse = sendTrig.send
-      p 'Send Status: ' + sendResponse.status.to_s
-      p 'Code: ' + sendResponse.code.to_s
-      p 'Message: ' + sendResponse.message.to_s
-      p 'Result Count: ' + sendResponse.results.length.to_s
-      p 'Results: ' + sendResponse.results.inspect
+        dataextensionrow = FuelSDK::DataExtension::Row.new
+        dataextensionrow.authStub = myclient
+        dataextensionrow.Name = 'WOOBOX_API'
+        dataextensionrow.props = {"EmailAddress" => entry['email'],
+                                  "Signup_Time" => entry['createdate'],
+                                  "Age" => entry['age'],
+                                  "City" => entry['city'],
+                                  "Class" => entry['class'],
+                                  "Custom4" => c4,
+                                  "Custom5" => c5,
+                                  "FName" => entry['first_name'],
+                                  "LName" => entry['last_name'],
+                                  "Phone" => entry['phone'],
+                                  "Occupation" => entry['occupation'],
+                                  "OtherProfession" => entry['other_profession'],
+                                  "IP_Address" => entry['ipaddress']}
+        results = dataextensionrow.post
 
+        p results.results
+
+        myclient = ET_Client.new auth
+
+        email = entry['email']
+
+    # Send an email with TriggeredSend
+        p '>>> Send an email with TriggeredSend'
+        sendTrig = FuelSDK::TriggeredSend.new
+        sendTrig.authStub = myclient
+        sendTrig.props = [{"CustomerKey" => "2156", "Subscribers" => {"SubscriberKey"=> email, "EmailAddress"=> email}}]
+        sendResponse = sendTrig.send
+        p 'Send Status: ' + sendResponse.status.to_s
+        p 'Code: ' + sendResponse.code.to_s
+        p 'Message: ' + sendResponse.message.to_s
+        p 'Result Count: ' + sendResponse.results.length.to_s
+        p 'Results: ' + sendResponse.results.inspect
+      end
     end
     render status: 200
   end
