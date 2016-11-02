3d88f3c0c6c6d944faef7e843abc16ba86e45a30
Nick-Brennan <nick_p_b@yahoo.com>
2016-11-02 11:04:05 -0700

    fixing status render

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -67,7 +67,7 @@ class MainController < ApplicationController
       p 'Results: ' + sendResponse.results.inspect
 
     end
-    render status: 200 nothing: true
+    render status: 200
   end
 
   def auth
